5714d5f6b6ab812d46fce4aedf2ac4a341fdabca
mariollesta <mario.llesta@proton.me>
2025-01-01 18:58:08 +0100

    feat(): card_detail component
    Custom card detail component.

---
+++ portfolio/components/card_detail.py
@@ -0,0 +1,28 @@
+import reflex as rx
+from portfolio.data import Extra
+from portfolio.styles.styles import IMAGE_HEIGHT, Size
+
+
+def card_detail(extra: Extra) -> rx.Component:
+    return rx.card(
+        rx.link(
+            rx.inset(
+                rx.image(
+                    src=extra.image,
+                    height=IMAGE_HEIGHT,
+                    width="100%",
+                    object_fit="cover"
+                ),
+                pb=Size.DEFAULT.value
+            ),
+            rx.text.strong(extra.title),
+            rx.text(
+                extra.description,
+                size=Size.SMALL.value,
+                color_scheme="gray"
+            )
+        ),
+        width="100%",
+        href=extra.url,
+        is_external=True
+    )
