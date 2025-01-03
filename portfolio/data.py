c670530a443aa3d409454c5ba0570345ce3f7de3
mariollesta <mario.llesta@proton.me>
2025-01-03 13:26:24 +0100

    fix(): data serialization
    Use Base from reflex.base (Pydantic).

--- portfolio/data.py
+++ portfolio/data.py
@@ -1,77 +1,59 @@
 import json
+from reflex.base import Base
+from typing import List, Optional
 
 
-class Media:
-    def __init__(self, email, cv, github, linkedin):
-        self.email = email
-        self.cv = cv
-        self.github = github
-        self.linkedin = linkedin
+class Media(Base):
+    email: str
+    cv: str
+    github: str
+    linkedin: str
 
 
-class Technology:
-    def __init__(self, icon, name):
-        self.icon = icon
-        self.name = name
+class Technology(Base):
+    icon: str
+    name: str
+
+        
+class Info(Base):
+    icon: str
+    title: str
+    subtitle: str
+    description: str
+    date: Optional[str] = ""
+    certificate: Optional[str] = ""
+    technologies: List[Technology] = []
+    image: Optional[str] = ""
+    url: Optional[str] = ""
+    github: Optional[str] = ""
 
 
-class Info:
-    def __init__(self, icon, title, subtitle, description, date="", certificate="", technologies=[], image="", url="", github=""):
-        self.icon = icon
-        self.title = title
-        self.subtitle = subtitle
-        self.description = description
-        self.date = date
-        self.certificate = certificate
-        self.technologies = [Technology(**tech) for tech in technologies]
-        self.image = image
-        self.url = url
-        self.github = github
+class Extra(Base):
+    image: str
+    title: str
+    description: str
+    url: str
 
 
-class Extra:
-    def __init__(self, image, title, description, url):
-        self.image = image
-        self.title = title
-        self.description = description
-        self.url = url
-
-
-class Data:
-    def __init__(
-        self,
-            title,
-            description,
-            image,
-            avatar,
-            name,
-            skill,
-            location,
-            media,
-            about,
-            technologies,
-            experience,
-            projects,
-            training,
-            extras
-    ):
-        self.title = title
-        self.description = description
-        self.image = image
-        self.avatar = avatar
-        self.name = name
-        self.skill = skill
-        self.location = location
-        self.media = Media(**media)
-        self.about = about
-        self.technologies = [Technology(**tech) for tech in technologies]
-        self.experience = [Info(**info) for info in experience]
-        self.projects = [Info(**info) for info in projects]
-        self.training = [Info(**info) for info in training]
-        self.extras = [Extra(**info) for info in extras]
-
+class Data(Base):
+    title: str
+    description: str
+    image: str
+    avatar: str
+    name: str
+    skill: str
+    location: str
+    media: Media
+    about: str
+    technologies: List[Technology]
+    experience: List[Info]
+    projects: List[Info]
+    training: List[Info]
+    extras: List[Extra]
+    
 
 with open("assets/data/data.json") as file:
     json_data = json.load(file)
+
 
 data = Data(**json_data)
